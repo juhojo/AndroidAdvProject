3d14fdb7dbcfff83f488bcdeb28b929312feed50
Juho Jokela <juho.i.jokela@gmail.com>
2016-09-24 15:00:27 +0300

    Updated MainActivity

--- UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/MainActivity.java
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
+    private UDgetTask asyncTask = new UDgetTask();
     TextView textView;
     User user;
 
@@ -23,14 +25,39 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
+        // If activity change was done correctly give user details
         if (extras != null) {
             username = extras.getString("username");
             team = extras.getString("team");
+
             user.setUsername(username);
             user.setTeam(team);
+            asyncTask.execute(username, "getId");
         }
 
         textView.setText(user.getUsername());
 
+        // Delegate back to this class
+        asyncTask.delegate = this;
+    }
+
+    @Override
+    public void processFinish(String output, String un, String team){
+        // Override interface method
+    }
+    @Override
+    public void udProcessFinish(String output, String id) {
+        if (output.equals("Success!")) {
+            user.setId(id);
+        } else {
+            user.setUsername(null);
+            user.setTeam(null);
+
+            Intent intent = new Intent(this, LoginActivity.class);
+
+            // Forward user's data
+            intent.putExtra("error", output);
+            startActivity(intent);
+        }
     }
 }
