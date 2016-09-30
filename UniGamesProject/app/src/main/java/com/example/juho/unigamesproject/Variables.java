de6c5b3e95522963e86900e864ccd3f573343088
Juho Jokela <juho.i.jokela@gmail.com>
2016-09-30 12:18:00 +0300

    Url for toornament

--- UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/Variables.java
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/Variables.java
@@ -7,6 +7,7 @@ public class Variables {
     // Urls
     final public static String URL_SIGN_UP = "http://users.metropolia.fi/~juhojo/folders/unigames/adduser.php";
     final public static String URL_USERS_JSON = "http://users.metropolia.fi/~juhojo/folders/unigames/users.json";
+    final public static String URL_TOORNAMENT = "http://www.toornament.com";
 
     // Preferences
     public static final String MyPREFERENCES = "MyPrefs";
