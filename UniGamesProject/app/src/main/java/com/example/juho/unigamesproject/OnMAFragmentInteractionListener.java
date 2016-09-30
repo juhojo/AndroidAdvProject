7b33f3ae27698a257406637005c8a0ffbc5032e2
Juho Jokela <juho.i.jokela@gmail.com>
2016-09-30 12:18:46 +0300

    Interface for FragmentInteractions

---
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/OnMAFragmentInteractionListener.java
@@ -0,0 +1,10 @@
+package com.example.juho.unigamesproject;
+
+import android.net.Uri;
+
+/**
+ * Created by Juho on 30.9.2016.
+ */
+public interface OnMAFragmentInteractionListener {
+    void onFragmentInteraction(Uri uri);
+}
