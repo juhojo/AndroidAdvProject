ff53b9503cabe6137fd43310c8087dbf2a9425f2
Juho Jokela <juho.i.jokela@gmail.com>
2016-10-02 15:09:03 +0300

    urls for scores (only scores.json) exits currently

--- UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/Variables.java
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/Variables.java
@@ -7,7 +7,12 @@ public class Variables {
     // Urls
     final public static String URL_SIGN_UP = "http://users.metropolia.fi/~juhojo/folders/unigames/adduser.php";
     final public static String URL_USERS_JSON = "http://users.metropolia.fi/~juhojo/folders/unigames/users.json";
+    final public static String URL_ADD_SCORES = "http://users.metropolia.fi/~juhojo/folders/unigames/addscores.php";
+    final public static String URL_GET_SCORES = "http://users.metropolia.fi/~juhojo/folders/unigames/scores.json";
+
     final public static String URL_TOORNAMENT = "http://www.toornament.com";
+    final public static String URL_TOORNAMENT_SCHEDULE = "";
+    final public static String URL_TOORNAMENT_RESULTS = "";
 
     // Preferences
     public static final String MyPREFERENCES = "MyPrefs";
