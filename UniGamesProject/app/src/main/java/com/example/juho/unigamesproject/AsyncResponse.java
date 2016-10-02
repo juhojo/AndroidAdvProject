ae3be8cc6d4b8e5e2f70683983a65683e5d94115
Juho Jokela <juho.i.jokela@gmail.com>
2016-10-02 15:04:56 +0300

    Add new method to AsyncResponse for JSONArray

--- UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/AsyncResponse.java
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/AsyncResponse.java
@@ -1,9 +1,12 @@
 package com.example.juho.unigamesproject;
 
+import org.json.JSONArray;
+
 /**
  * Created by Juho on 19.9.2016.
  */
 public interface AsyncResponse {
     void processFinish(String output, String un, String team);
     void udProcessFinish(String output, String id);
+    void ttProcessFinish(JSONArray jsonArray);
 }
