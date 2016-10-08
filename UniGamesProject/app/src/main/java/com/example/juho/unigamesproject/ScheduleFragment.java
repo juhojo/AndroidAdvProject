8b1661a03c2d77817db60b3b546aaa645c4f09da
Juho Jokela <juho.i.jokela@gmail.com>
2016-10-08 11:43:05 +0300

    Start to work with populating the schedule

--- UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/ScheduleFragment.java
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/ScheduleFragment.java
@@ -7,38 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import org.json.JSONArray;
 
 
-public class ScheduleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ScheduleFragment extends Fragment implements AsyncResponse {
 
     private OnMAFragmentInteractionListener mListener;
+    ListView listView;
+
+    private ToornamentTask asyncTask;
+    CustomAdapter adapter;
 
     public ScheduleFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleFragment newInstance(String param1, String param2) {
+    public static ScheduleFragment newInstance() {
         ScheduleFragment fragment = new ScheduleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -47,19 +36,33 @@ public class ScheduleFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Create new instance of ToornamentTask
+        asyncTask = new ToornamentTask();
+        asyncTask.execute("get", "schedule"); // populate the list
+
+        // Delegate back to this class
+        asyncTask.delegate = this;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        final RelativeLayout myView = (RelativeLayout)inflater.inflate(R.layout.fragment_schedule, container, false);
+        listView = (ListView)myView.findViewById(R.id.schedule_list);
+        return myView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -83,4 +86,20 @@ public class ScheduleFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void processFinish(String output, String un, String team){
+        // Override interface method
+    }
+    @Override
+    public void udProcessFinish(String output, String id) {
+        // Override interface method
+    }
+    @Override
+    public void ttProcessFinish(JSONArray jsonArray){
+        System.out.println("ScheduleFragment gets jsonarray: " + jsonArray);
+        /*
+        JSONSorter jsonSorter = new JSONSorter(this.getContext());
+        jsonSorter.execute(jsonArray);
+        */
+    }
 }
