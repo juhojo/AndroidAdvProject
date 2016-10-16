572cb1a2bfb9a5f0a641fe014d120f087a3d4a20
Juho Jokela <juho.i.jokela@gmail.com>
2016-10-16 12:56:53 +0300

    AsyncTask for bets

---
+++ UniGamesProject/app/src/main/java/com/example/juho/unigamesproject/BetTask.java
@@ -0,0 +1,29 @@
+package com.example.juho.unigamesproject;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Juho on 12.10.2016.
+ */
+public class BetTask extends AsyncTask<String, String, String> {
+    public BetResponse delegate = null;
+    private Bet betObject;
+
+    BetTask(Bet betObject) {
+        this.betObject = betObject;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String message = "Failed";
+
+        if (betObject != null) {
+            message = HttpManager.setBet(betObject);
+        }
+        return message;
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.betProcessFinish(result);
+    }
+}
